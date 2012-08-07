0048734d892fdcb1a7843265bed79044b28f9ea5
FrancisVarga <github@varga-net.com>
2012-08-07 15:01:27 +0200

    what ever

--- htdocs/api/v1/app/index.php
+++ htdocs/api/v1/app/index.php
@@ -1,15 +1,7 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: thelittlenerd87
- * Date: 5/21/12
- * Time: 3:26 PM
- * To change this template use File | Settings | File Templates.
- */
 
-
-$processusCorePath = '../core/';
-$applicationPath   = '../../../application/php/Application/';
+$processusCorePath = '../../../../library/Processus/core/';
+$applicationPath   = '../../../../application/php/Application/';
 
 require_once($processusCorePath . 'Interfaces/InterfaceBootstrap.php');
 require_once($processusCorePath . 'Interfaces/InterfaceApplicationContext.php');
@@ -18,3 +10,9 @@ require_once($applicationPath . 'ApplicationBootstrap.php');
 
 $bootstrap = \Application\ApplicationBootstrap::getInstance();
 $bootstrap->init();
+$gtw = new Application\JsonRpc\V1\App\Gateway();
+$bootstrap->setGateway($gtw);
+$gtw->run();
+
+
+?>
