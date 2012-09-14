718f3a769e77e5bf34b467fb2ff0a0909172618a
Francis Varga <github@varga-net.com>
2012-09-15 00:06:06 +0200

    remove pre set Exception

--- core/Exceptions/JsonRpc/ValidJsonRpcRequest.php
+++ core/Exceptions/JsonRpc/ValidJsonRpcRequest.php
@@ -6,22 +6,7 @@
  * Time: 3:48 PM
  * To change this template use File | Settings | File Templates.
  */
-namespace Processus\Exceptions\JsonRpc
+namespace Processus\Exceptions\JsonRpc;
+class ValidJsonRpcRequest extends \Processus\Exceptions\ProcessusException
 {
-
-    class ValidJsonRpcRequest extends \Processus\Exceptions\ProcessusException
-    {
-        /**
-         * @param string $message
-         * @param string $code
-         * @param string $severity
-         * @param string $filename
-         * @param int    $lineno
-         * @param        $previous
-         */
-        public function __construct($message = "", $code = "PRC-1000", $severity = "10", $filename = __FILE__, $lineno = __LINE__, $previous)
-        {
-            parent::__construct($message, $code, $severity, $filename, $lineno, $previous);
-        }
-    }
 }
