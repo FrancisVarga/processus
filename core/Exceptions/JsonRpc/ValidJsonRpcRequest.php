1d18a83cab3eb45ce86ae0dc4218ee2b4af21f8f
Francis Varga <github@varga-net.com>
2012-10-04 23:13:32 +0200

    remove constructor

--- core/Exceptions/JsonRpc/ValidJsonRpcRequest.php
+++ core/Exceptions/JsonRpc/ValidJsonRpcRequest.php
@@ -11,17 +11,6 @@ namespace Processus\Exceptions\JsonRpc
 
     class ValidJsonRpcRequest extends \Processus\Exceptions\ProcessusException
     {
-        /**
-         * @param string $message
-         * @param string $code
-         * @param string $severity
-         * @param string $filename
-         * @param int    $lineno
-         * @param        $previous
-         */
-        public function __construct($message = "", $code = "PRC-1000", $severity = "10", $filename = __FILE__, $lineno = __LINE__, $previous)
-        {
-            parent::__construct($message, $code, $severity, $filename, $lineno, $previous);
-        }
+
     }
 }
