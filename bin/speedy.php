fe3745d3511256283b77bb24faa32a93fdcb52ab
FrancisVarga <github@varga-net.com>
2012-11-20 20:43:18 +0100

    adding vendor autloading

--- bin/speedy.php
+++ bin/speedy.php
@@ -1,12 +1,9 @@
 #!/usr/bin/php -d memory_limit=536870912 -f
 
 <?php
-$processusCorePath = '../core/';
-$applicationPath   = '../../../application/php/Application/';
 
-require_once($processusCorePath . 'Interfaces/InterfaceBootstrap.php');
-require_once($processusCorePath . 'ProcessusBootstrap.php');
-require_once($applicationPath . 'ApplicationBootstrap.php');
+require __DIR__ . "/../vendor/autoload.php";
 
+?>
 \Application\ApplicationBootstrap::getInstance()->init("TASK");
 \Processus\Task\Runner::run();
